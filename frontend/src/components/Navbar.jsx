d18ad25185cd4b872ab3fc9129b8f54c7aecd0e3
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-07-13 21:03:13 +0530

    Trailer section update, movies page and favourite page done, navbar bug fix, footer update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <Link
         to={'/'}    
         className='max-md:flex-1'>
-        <img src={assets} alt='logo' className='w-36 h-auto' />
+        <img src={assets.logo} alt='logo' className='w-36 h-auto' />
         </Link>
 
         <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Herosection from '../components/Herosection'
 import FeatureSection from '../components/FeatureSection'
+import TrailersSection from '../components/TrailersSection'
 
 const Home = () => {
   return (
     <div>
       <Herosection/>
       <FeatureSection/>
+      <TrailersSection/>
       
     </div>
   )
